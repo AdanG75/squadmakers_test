46fafdbeac792bbc7d0c84777cbb9f8803dac97c
AdanG75 <ansg75@gmail.com>
2023-04-08 19:52:44 -0600

    Add docstring to entry points

--- routers/joke_router.py
+++ routers/joke_router.py
@@ -18,9 +18,16 @@ router = APIRouter(
     path='/',
     status_code=status.HTTP_200_OK,
     response_model=Joke,
-    tags=['jokes']
+    tags=['jokes'],
+    summary="Get random Joke"
 )
 async def get_random_joke():
+    """
+    GET a random joke from Dad or Chuck repository
+
+    **Response**
+    - Return a response body of type \'Joke\' with status code 200
+    """
     response = joke_controller.get_random_joke()
 
     return response
@@ -30,11 +37,21 @@ async def get_random_joke():
     path='/{repository}/',
     status_code=status.HTTP_200_OK,
     response_model=Joke,
-    tags=['jokes']
+    tags=['jokes'],
+    summary='Get random joke based on repository'
 )
 async def get_random_joke_by_repository(
         repository: JokeFrom = Path(...)
 ):
+    """
+    GET a random joke based on the repository passed as path parameter
+
+    **Path parameter**
+    - repository (Dad | Chuck): String with value \'Chuck\' or \'Dad\' which specify the repository where system fetch
+
+    **Response**
+    - Return a response body of type \'Joke\' with status code 200
+    """
     response = joke_controller.get_random_joke(repository)
 
     return response
@@ -44,12 +61,23 @@ async def get_random_joke_by_repository(
     path='/',
     status_code=status.HTTP_201_CREATED,
     response_model=Joke,
-    tags=['jokes']
+    tags=['jokes'],
+    summary='Save Joke'
 )
 async def save_joke(
         joke: str = Query(None, min_length=4, max_length=999),
         db: Session = Depends(get_db)
 ):
+    """
+    POST a joke into the system
+
+    **Query parameter**
+    - joke(str): String which represents the joke to be saved
+
+    **Response**
+    - Return a response body of type \'Joke\' with the joke sent it as query parameter. As the same way,
+    the response body have a status code 201
+    """
     if joke is None:
         return joke_controller.get_random_joke()
 
@@ -62,13 +90,27 @@ async def save_joke(
     path='/{number}/',
     status_code=status.HTTP_200_OK,
     response_model=Joke,
-    tags=['jokes']
+    tags=['jokes'],
+    summary='Update joke'
 )
 async def update_joke(
         number: int = Path(..., gt=0, lt=4294967296),
         joke: str = Query(None, min_length=4, max_length=999),
         db: Session = Depends(get_db)
 ):
+    """
+    PATCH a saved joke with id equal to \'number\' and replacing the joke using the passed as query parameter
+
+    **Path parameter**
+    - number(int): Integer number which represents the identifier of the joke
+
+    **Query parameter**
+    - joke(str): The text of the joke which replace the old joke saved
+
+    **Response**
+    - Return a response body of type \'Joke\' with the joke sent it as query parameter. As the same way,
+    the response body have a status code 200
+    """
     response = joke_controller.update_joke(db, number, joke)
 
     return response
@@ -78,12 +120,22 @@ async def update_joke(
     path='/{number}/',
     status_code=status.HTTP_200_OK,
     response_model=BasicResponse,
-    tags=['jokes']
+    tags=['jokes'],
+    summary='Delete joke'
 )
 async def delete_joke(
         number: int = Path(..., gt=0, lt=4294967296),
         db: Session = Depends(get_db)
 ):
+    """
+    Delete a saved joke with id equal to \'number\'
+
+    **Path parameter**
+    - number(int): Integer number which represents the identifier of the joke
+
+    **Response**
+    - A response body of type **Basic Response** and status code with value 200
+    """
     result: bool = joke_controller.delete_joke(db, number)
 
     return BasicResponse(
--- routers/number_router.py
+++ routers/number_router.py
@@ -15,11 +15,21 @@ router = APIRouter(
 @router.get(
     path='/mcm/',
     status_code=status.HTTP_200_OK,
-    response_model=NumberResponse
+    response_model=NumberResponse,
+    summary='Least Common Multiple'
 )
 async def get_least_common_multiple(
         numbers: List[int] = Query(None)
 ):
+    """
+    GET the least common multiple of all numbers passed as query parameters
+
+    **Query parameter**
+    - numbers(List[int]): List of integers numbers
+
+    **Response**
+    - Response body of type **NumberResponse** with status code 200
+    """
     if numbers is not None and len(numbers) > 0:
         result: int = get_lcm_of_a_integer_list(numbers)
     else:
@@ -34,11 +44,21 @@ async def get_least_common_multiple(
 @router.get(
     path='/add-one/{number}/',
     status_code=status.HTTP_200_OK,
-    response_model=NumberResponse
+    response_model=NumberResponse,
+    summary='Plus one'
 )
 async def add_one(
         number: int = Path(...)
 ):
+    """
+    GET the value of \'number\' plus one
+
+    **Path parameter**
+    - number(int): Value which the system will add one
+
+    **Response**
+    - Response body of type **NumberResponse** with status code 200
+    """
     result: int = add_one_c(number)
 
     return NumberResponse(
